5aa5c257b75987a8b04eb56707fa406f8e8e051b
Joey Rosztoczy <joeyr@stotzeq.com>
2014-07-25 15:54:06 -0500

    test skeleton and dependencies for redis tests

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
   # resources :tweets, only: {index}
   # You can have the root of your site routed with "root"
-  root 'tweet#index'
+  root 'tweets#index'
 
   mount Sidekiq::Web => '/sidekiq'
 
