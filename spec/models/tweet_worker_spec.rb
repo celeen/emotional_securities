2ca64836b8b70753c22f07979ca91170947fb8cb
celeen <celeenrusk@gmail.com>
2014-07-25 17:54:37 -0500

    some leftover testing folders that didn't get added b4

---
+++ spec/models/tweet_worker_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+describe 'TweetWorker'
+	context '#perform' do 
+	let(:tweet_worker) { TweetWorker.new }
+		it "should accept a tweet_id" do
+		end
+
+		xit "should find a tweet object" do
+		end
+
+		xit "should instantiate a new AlchemyAPI connection" do
+		end
+
+		xit "should return a response from the AlchemyAPI" do
+		end
+
+		it "should return a tweet object that has a sentiment value" do
+		end
+	end
