8aaa748e7c60069b8d32ee8df64a0597967a7679
celeen <celeenrusk@gmail.com>
2014-07-26 19:25:30 -0500

    changed tests to reflect new structure

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,8 +1,7 @@
 require 'rails_helper'
 
 describe Article, :type => :model do
-  let (:company) {Company.create(name:"apple", symbol:"aapl")}
-  let (:article) {company.articles.create(url: "http://seekingalpha.com/article/2344175-teslas-real-problem-energy-density", sentiment: 0)}
+  let (:article) {Article.create(url: "http://seekingalpha.com/article/2344175-teslas-real-problem-energy-density", sentiment: 0, company: 'aapl')}
   context "in attributes" do
 
     it "should have a url" do
@@ -12,11 +11,8 @@ describe Article, :type => :model do
     it "should have a sentiment" do
       expect(article.sentiment).to eq(0)
     end
-  end
-
-  context "associations" do
-    it "should be embedded in company" do
-      expect(article.company.name).to eq("apple")
+    it "should have a company" do
+      expect(article.company).to eq("aapl")
     end
   end
 
