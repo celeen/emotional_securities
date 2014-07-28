02f0be186377c6cebad53babd9d6c24c202f9b00
celeen <celeenrusk@gmail.com>
2014-07-28 15:06:09 -0500

    better layoutz

--- app/assets/javascripts/sentiment.js
+++ app/assets/javascripts/sentiment.js
@@ -1,88 +1,97 @@
 Array.max = function(array) {
-	return Math.max.apply( Math, array);
+    return Math.max.apply(Math, array);
 }
 
 Array.min = function(array) {
-	return Math.min.apply( Math, array);
+    return Math.min.apply(Math, array);
 }
 
 function stockChart(data) {
-	console.log(data)
-	chart = c3.generate({
-    data: {
-    	x: 'dates',
-    	json: {
-        		dates: data.dates,
-            tweetSentiment: data.tweetSentiments,
-            prices: data.prices,
-            volume: data.volume,
-   		},
-    	axes: {
-    		tweetSentiment: 'y',
-    		prices: 'y2',
-    		volume: 'y2'
-    	},
-        types: {
-            prices: 'spline',
-            tweetSentiment: 'spline',
-            volume: 'bar'
+    console.log(data)
+    chart = c3.generate({
+        size: {
+            height: 550,
+
         },
-    },
-    subchart: {
-    	show: true
-    },
-    axis: {
-    	x: {
-    		type: 'timeseries',
-    		tick: {
-    			count: 4,
-    			format: '%m/%d/%y'
-    		},
-    	},
-    	y: {
-    		label: "Sentiment",
-    		min: -1,
-    		max: 1,
-    	},
-    	y2: {
-    		label: "Share Price",
-    		show: true,
-    		padding: {top:Array.max(data.prices)*.01, bottom:Array.min(data.prices)*.01},
-    	},
-    	y3: {
-    		show: true
-    	}
-    },
-    tooltip: {
-      format: {
-          title: function (d) { return 'Data ' + d; },
-          value: function (value, ratio, id) {
-              var format = id === 'prices' ? d3.format('$') : d3.format('.');
-              return format(value);
-          }
-      }
-  }
-	});
-	chart.hide('volume');
+        data: {
+            x: 'dates',
+            json: {
+                dates: data.dates,
+                tweetSentiment: data.tweetSentiments,
+                prices: data.prices,
+                volume: data.volume,
+            },
+            axes: {
+                tweetSentiment: 'y',
+                prices: 'y2',
+                volume: 'y2'
+            },
+            types: {
+                prices: 'spline',
+                tweetSentiment: 'spline',
+                volume: 'bar'
+            },
+        },
+        subchart: {
+            show: true
+        },
+        axis: {
+            x: {
+                type: 'timeseries',
+                tick: {
+                    count: 4,
+                    format: '%m/%d/%y'
+                },
+            },
+            y: {
+                label: "Sentiment",
+                min: -1,
+                max: 1,
+            },
+            y2: {
+                label: "Share Price",
+                show: true,
+                padding: {
+                    top: Array.max(data.prices) * .01,
+                    bottom: Array.min(data.prices) * .01
+                },
+            },
+            y3: {
+                show: true
+            }
+        },
+        tooltip: {
+            format: {
+                title: function(d) {
+                    return 'Data ' + d;
+                },
+                value: function(value, ratio, id) {
+                    var format = id === 'prices' ? d3.format('$') : d3.format('.');
+                    return format(value);
+                }
+            }
+        }
+    });
+    chart.hide('volume');
 }
 
-function populateBoxes(boxData){
-	console.log(boxData);
-	$(".1").append("<p> Daily Expert Sentiment </p>"+ boxData.avg_daily_expert_sentiment + "</p>")
+function populateBoxes(boxData) {
+    console.log(boxData);
+    $(".1").append("<p> Daily Expert Sentiment </p>" + boxData.avg_daily_expert_sentiment + "</p>")
 }
 
-$(document).ready(function(){
-	var chart, volume, prices, tweetSentiment, articleSentiment, stockData;
 
+$(document).ready(function() {
+    var chart, volume, prices, tweetSentiment, articleSentiment, stockData;
 
-	$.post('/chart_data', function(response){
-		stockData = response;
-		stockChart(stockData);
-	}, 'json');
+    $.post('/chart_data', function(response) {
+        stockData = response;
+        stockChart(stockData);
+    }, 'json');
 
-	$.post('/box_data', function(response){
-		boxData=response;
-		populateBoxes(boxData);
-	}, 'json');
+    $.post('/box_data', function(response) {
+        boxData = response;
+        populateBoxes(boxData);
+    }, 'json');
 
 });
