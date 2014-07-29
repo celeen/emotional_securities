51073b65d9c4f19ecfb367b8aed1462517f856a2
Joey Rosztoczy <joeyr@stotzeq.com>
2014-07-29 11:30:09 -0500

    conflict resolution

--- app/assets/javascripts/sentiment.js
+++ app/assets/javascripts/sentiment.js
@@ -2,64 +2,6 @@ Array.max = function(array) {
     return Math.max.apply(Math, array);
 }
 
-<<<<<<< HEAD
-	function stockChart(data) { 
-		console.log(data)
-		chart = c3.generate({
-	    data: {
-	    	x: 'dates',
-	    	json: {
-	        		dates: data.dates,
-	            tweetSentiment: data.tweetSentiments,
-	            prices: data.prices,
-	            volume: data.volume,
-	    	},
-	    	axes: {
-	    		tweetSentiment: 'y',
-	    		prices: 'y2',
-	    		volume: 'y2'
-	    	},
-	        types: {
-	            prices: 'spline',
-	            tweetSentiment: 'line',
-	            volume: 'bar'
-	        },
-	    },
-	    subchart: {
-	    	show: true
-	    },
-	    axis: {
-	    	x: {
-	    		type: 'timeseries',
-	    		tick: {
-	    			count: 4,
-	    			format: '%m/%d/%y'
-	    		}
-	    	},
-	    	y: {
-	    		label: "Sentiment",
-	    		min: -1,
-	    		max: 1,
-	    	},
-	    	y2: {
-	    		label: "Share Price",
-	    		show: true,
-	    		padding: {
-	    			top: Array.max(data.prices),
-	    			bottom: 0,
-	    		}
-	    	},
-	    	y3: {
-	    		show: true
-	    	}
-	    },
-	    tooltip: {
-        format: {
-            title: function (d) { return 'Data ' + d; },
-            value: function (value, ratio, id) {
-                var format = id === 'prices' ? d3.format('$') : d3.format('.');
-                return format(value);
-=======
 Array.min = function(array) {
     return Math.min.apply(Math, array);
 }
@@ -127,7 +69,6 @@ function stockChart(data) {
                     var format = id === 'prices' ? d3.format('$') : d3.format('.');
                     return format(value);
                 }
->>>>>>> 033645aad44d5ed8c3dc86bcc48cb0bd5eb0493f
             }
         }
     });
