0aff0aac00e6f81c90043825cf0e192368f61518
Joey Rosztoczy <joeyr@stotzeq.com>
2014-07-29 19:46:28 -0500

    stash running average

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -6,4 +6,24 @@ class Tweet
   field :sentiment, type: Float
   field :tweeted_at, type: DateTime
   field :company, type: String
+
+
+
+  def running_avg(company, limit)
+  	# ordered = Tweet.where(company: company).order{:tweeted_at}
+  	# ordered.map { |tweet| }
+  	# .limit(limit).avg(:sentiment)
+
+  	# array[tweet.postion...tweet.position+10]
+  	# avg = 0.0
+  	# count = 0
+
+  	# lamda do |x| 
+  	# 	count += 1
+  	# 	avg += (x - avg) / count
+  	# end
+
+  end
 end
+
+# in controller dataPoints.map { |point| point.running_10 }
