8d1228a6e5de2ca2bf7dba8824c6bfa70c83f0c7
celeen <celeenrusk@gmail.com>
2014-07-30 17:51:19 -0500

    this ajaxing should work...

--- app/assets/javascripts/high_chart.js
+++ app/assets/javascripts/high_chart.js
@@ -1,187 +1,210 @@
+<< << << < HEAD
 var chart, volume, prices, tweetSentiment, articleSentiment, stockData;
 
 function highChart(data) {
     $('#container').highcharts({
-        chart: {
-            backgroundColor: '#EEEFDF',
-            type: 'line',
-            style: {
-                font: '12pt News Cycle, serif',
-                color: '#0f1c13'
-            },
-        },
-        title: {
-            text: 'Market Feelz And your moneys',
-            style: {
-                font: '20pt News Cycle, serif',
-                color: '#0F1C13'
-            },
-        },
-        subtitle: {
-            text: 'watch the emo secs',
-            style: {
-                font: '14pt News Cycle, serif',
-            }
-        },
-        xAxis: {
-            type: 'datetime',
-            dateTimeLabelFormats: { // don't display the dummy year
-                hour: '%l:%M %p',
-                ///day: '%a',
-                //month: '%e1 %b',
-                //year: '%b'
+            chart: {
+                backgroundColor: '#EEEFDF',
+                type: 'line',
+                style: {
+                    font: '12pt News Cycle, serif',
+                    color: '#0f1c13'
+                },
             },
             title: {
-                text: 'The Last 24 Hours'
-            },
-            max: Date.now(),
-            min: Date.now() - 86400000,
-        },
-        yAxis: [{ //sentiment axis
-            labels: {
+                text: 'Market Feelz And your moneys',
                 style: {
-                    color: '#7F8FC3'
-                }
+                    font: '20pt News Cycle, serif',
+                    color: '#0F1C13'
+                },
             },
-            title: {
-                text: 'Tweet Sentiment',
+            subtitle: {
+                text: 'watch the emo secs',
                 style: {
-                    color: '#3C528B',
+                    font: '14pt News Cycle, serif',
                 }
             },
-            min: -1,
-            max: 1,
-            opposite: true,
-        }, { //Price Axis
-            labels: {
-                style: {
-                    color: '#BD872D'
+            xAxis: {
+                type: 'datetime',
+                dateTimeLabelFormats: { // don't display the dummy year
+                    hour: '%l:%M %p',
+                    ///day: '%a',
+                    //month: '%e1 %b',
+                    //year: '%b'
+                    === === =
+                        function highChart(data) {
+                            $('#container').highcharts({
+                                    chart: {
+                                        backgroundColor: '#EEEFDF',
+                                        type: 'line',
+                                        style: {
+                                            font: '12pt News Cycle, serif',
+                                            color: '#0f1c13'
+                                        },
+                                        >>> >>> > this ajaxing should work...
+                                    },
+                                    title: {
+                                        text: 'The Last 24 Hours'
+                                    },
+                                    max: Date.now(),
+                                    min: Date.now() - 86400000,
+                                },
+                                yAxis: [{ //sentiment axis
+                                    labels: {
+                                        style: {
+                                            color: '#7F8FC3'
+                                        }
+                                    },
+                                    title: {
+                                        text: 'Tweet Sentiment',
+                                        style: {
+                                            color: '#3C528B',
+                                        }
+                                    },
+                                    min: -1,
+                                    max: 1,
+                                    opposite: true,
+                                }, { //Price Axis
+                                    labels: {
+                                        style: {
+                                            color: '#BD872D'
+                                        }
+                                    },
+                                    title: {
+                                        text: 'Price',
+                                        style: {
+                                            color: '#BD872D'
+                                        }
+                                    }
+                                }],
+
+                                series: [{
+
+                                    name: 'Tweet Sentiment',
+                                    data: data.tweets,
+                                    id: 'primary',
+                                    type: 'scatter',
+                                    color: '#7F8FC3',
+                                    tooltip: {
+                                        headerFormat: '<b>{series.name}</b><br>',
+                                        pointFormat: '{point.x:%l:%M %p}: {point.y:.4f}',
+                                    },
+                                }, {
+                                    name: 'Price',
+                                    data: data.prices,
+                                    shadow: true,
+                                    yAxis: 1,
+                                    color: '#BD872D',
+                                    tooltip: {
+                                        headerFormat: '<b>{series.name}</b><br>',
+                                        pointFormat: '{point.x:%l:%M %p}: ${point.y:.2f}',
+                                    },
+
+                                }, {
+                                    connectNulls: true,
+                                    shadow: true,
+                                    name: 'Average Tweet Sentiment',
+                                    linkedTo: 'primary',
+                                    showInLegend: true,
+                                    type: 'trendline',
+                                    algorithm: 'EMA',
+                                    periods: 50,
+                                    color: '#3C528B',
+                                    tooltip: {
+                                        headerFormat: '<b>Average Sentiment</b><br>',
+                                        pointFormat: '{point.y:.4f}',
+                                    },
+                                }],
+                                plotOptions: {
+                                    scatter: {
+                                        marker: {
+                                            radius: 1,
+                                            states: {
+                                                hover: {
+                                                    enabled: true,
+                                                    lineColor: '#3C528B'
+                                                }
+                                            }
+                                        }
+                                    }
+                                },
+                                legend: {
+                                    itemStyle: {
+                                        font: '12pt News Cycle, serif',
+                                        color: '#0F1C13',
+                                    }
+                                },
+                                navigation: {
+                                    buttonOptions: {
+                                        enabled: false
+                                    }
+                                }
+                            });
                 }
-            },
-            title: {
-                text: 'Price',
-                style: {
-                    color: '#BD872D'
+
+                function populateVolumeBox(volume, box, label) {
+                    $(box).append("<p>" + label + " </p>" + volume + "</p>")
                 }
-            }
-        }],
 
-        series: [{
-
-            name: 'Tweet Sentiment',
-            data: data.tweets,
-            id: 'primary',
-            type: 'scatter',
-            color: '#7F8FC3',
-            tooltip: {
-                headerFormat: '<b>{series.name}</b><br>',
-                pointFormat: '{point.x:%l:%M %p}: {point.y:.4f}',
-            },
-        }, {
-            name: 'Price',
-            data: data.prices,
-            shadow: true,
-            yAxis: 1,
-            color: '#BD872D',
-            tooltip: {
-                headerFormat: '<b>{series.name}</b><br>',
-                pointFormat: '{point.x:%l:%M %p}: ${point.y:.2f}',
-            },
-
-        }, {
-            connectNulls: true,
-            shadow: true,
-            name: 'Average Tweet Sentiment',
-            linkedTo: 'primary',
-            showInLegend: true,
-            type: 'trendline',
-            algorithm: 'EMA',
-            periods: 50,
-            color: '#3C528B',
-            tooltip: {
-                headerFormat: '<b>Average Sentiment</b><br>',
-                pointFormat: '{point.y:.4f}',
-            },
-        }],
-        plotOptions: {
-            scatter: {
-                marker: {
-                    radius: 1,
-                    states: {
-                        hover: {
-                            enabled: true,
-                            lineColor: '#3C528B'
-                        }
-                    }
+                function populateSentimentBoxes(sentiment, r, box, label) {
+                    $(box).append("<p>" + label + " </p><p> Feelz: " + sentiment + "</p><p> R: " + r + "</p><p> R-Squared: " + Math.round(r * r * 100) / 100 + "</p>")
                 }
-            }
-        },
-        legend: {
-            itemStyle: {
-                font: '12pt News Cycle, serif',
-                color: '#0F1C13',
-            }
-        },
-        navigation: {
-            buttonOptions: {
-                enabled: false
-            }
-        }
-    });
-}
 
-function populateVolumeBox(volume, box, label) {
-    $(box).append("<p>" + label + " </p>" + volume + "</p>")
-}
-
-function populateSentimentBoxes(sentiment, r, box, label) {
-    $(box).append("<p>" + label + " </p><p> Feelz: " + sentiment + "</p><p> R: " + r + "</p><p> R-Squared: " + Math.round(r * r * 100) / 100 + "</p>")
-}
+                function getChartData(symbol) {
+                    console.log('getting chart data!')
+                    $.post('/chart_data', {
+                            'company': symbol
+                        },
+                        stockChart,
+                        'json');
+                };
 
 
-$(document).ready(function() {
-    getChartData('AAPL');
+                function getExpertData(symbol) {
+                    $.post('/expert_data', {
+                        company: symbol
+                    }, function(response) {
+                        var expert_data = response;
+                        var expert_label = "Experts";
+                        populateSentimentBoxes(expert_data.avg_daily_expert_sentiment, expert_data.correlation, '.feature.expert h4', expert_label);
+                    }, 'json');
+                };
+
+                function getHerdData(symbol) {
+                    $.post('/herd_data', {
+                        company: symbol
+                    }, function(response) {
+                        var herd_data = response;
+                        var herd_label = "Herd";
+                        populateSentimentBoxes(herd_data.avg_daily_herd_sentiment, herd_data.correlation, '.feature.herd h4', herd_label);
+                    }, 'json');
+                };
+
+                function getVolumeData(symbol) {
+                    $.post('/volume_data', {
+                        company: symbol
+                    }, function(response) {
+                        var volume_data = response;
+                        console.log(volume_data.daily_volume_delta);
+                        var volume_label = "Volume Delta";
+                        populateVolumeBox(volume_data.daily_volume_delta, '.feature.volume h4', volume_label);
+                    }, 'json');
+                };
 
 
-    function getChartData(symbol) {
-        console.log('getting chart data!')
-        $.post('/chart_data', {
-                'company': symbol
-            },
-            highChart,
-            'json');
-    };
-    // $.post('/chart_data', function(response) {
-    //     stockData = response;
-    //     highChart(stockData);
-    //     console.log(stockData.tweets)
-    // }, 'json');
+                $(document).ready(function() {
+                    var chart, volume, prices, tweetSentiment, articleSentiment, stockData;
 
-    var company = 'AAPL'
+                    $('body').on("click", '.stocks > li',
+                        function(event) {
+                            event.preventDefault();
+                            console.log(event);
+                            console.log(event.currentTarget.className);
+                            var symbol = event.currentTarget.className;
+                            getVolumeData(symbol);
+                            getHerdData(symbol);
+                            getExpertData(symbol);
+                            getChartData(symbol);
+                        });
 
-    $.post('/expert_data', {
-        company: company
-    }, function(response) {
-        var expert_data = response;
-        var expert_label = "Experts";
-        populateSentimentBoxes(expert_data.avg_daily_expert_sentiment, expert_data.correlation, '.feature.expert h4', expert_label);
-    }, 'json');
-
-    $.post('/herd_data', {
-        company: company
-    }, function(response) {
-        var herd_data = response;
-        var herd_label = "Herd";
-        populateSentimentBoxes(herd_data.avg_daily_herd_sentiment, herd_data.correlation, '.feature.herd h4', herd_label);
-    }, 'json');
-
-    $.post('/volume_data', {
-        company: company
-    }, function(response) {
-        var volume_data = response;
-        console.log(volume_data.daily_volume_delta);
-        var volume_label = "Volume Delta";
-        populateVolumeBox(volume_data.daily_volume_delta, '.feature.volume h4', volume_label);
-    }, 'json');
-
-});
+                });
