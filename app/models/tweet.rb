8c5cf4117f33ff3e9480273db65ec236279f4a20
celeen <celeenrusk@gmail.com>
2014-07-31 12:45:14 -0500

    cleaning up

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -12,14 +12,5 @@ class Tweet
     self.inject{ |sum, n| sum + n.sentiment }/self.length
   end
 
-  def self.running10(company)
-     moving_avg = []
-     ordered_sentiments = Tweet.where(company: company).order{:tweeted_at}.pluck(:sentiment)
-
-     p ordered_sentiments.length.times {|i| moving_avg << ordered_sentiments[i...i+10].inject(:+)/10 unless ordered_sentiments[i+10] == nil}
-
-     p moving_avg
-  end
-
 end
 
