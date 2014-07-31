95e371f2829d3f8e428b5404d57bc2b149078b76
Xander Glassman <x@x.c>
2014-07-30 19:32:38 -0500

    d

--- app/assets/javascripts/high_chart.js
+++ app/assets/javascripts/high_chart.js
@@ -15,6 +15,13 @@ function highChart(data) {
                 font: '12pt News Cycle, serif',
                 color: '#0f1c13'
             },
+            zoomType: 'x',
+
+        },
+        resetZoomButton: {
+            style: {
+                width: 400,
+            },
         },
         title: {
             text: 'Market Feelz And your moneys',
@@ -148,6 +155,9 @@ function populateSentimentBoxes(sentiment, r, box, label) {
 $(document).ready(function() {
     var chart, volume, prices, tweetSentiment, articleSentiment, stockData;
 
+    $('#resetZoom').click(function() {
+        chart.zoomOut();
+    });
 
     $.post('/chart_data', function(response) {
         stockData = response;
@@ -157,19 +167,25 @@ $(document).ready(function() {
 
     var company = 'AAPL'
 
-    $.post('/expert_data', { company: company }, function(response) {
+    $.post('/expert_data', {
+        company: company
+    }, function(response) {
         var expert_data = response;
         var expert_label = "Experts";
         populateSentimentBoxes(expert_data.avg_daily_expert_sentiment, expert_data.correlation, '.feature.expert h4', expert_label);
     }, 'json');
 
-    $.post('/herd_data', { company: company }, function(response) {
+    $.post('/herd_data', {
+        company: company
+    }, function(response) {
         var herd_data = response;
         var herd_label = "Herd";
         populateSentimentBoxes(herd_data.avg_daily_herd_sentiment, herd_data.correlation, '.feature.herd h4', herd_label);
     }, 'json');
 
-    $.post('/volume_data', { company: company }, function(response) {
+    $.post('/volume_data', {
+        company: company
+    }, function(response) {
         var volume_data = response;
         console.log(volume_data.daily_volume_delta);
         var volume_label = "Volume Delta";
